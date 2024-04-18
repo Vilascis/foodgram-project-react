8d7a83a6a511fcbd20d4157bcb58886af83a7be2
Иван Шляков <vilascis88@gmail.com>
2024-04-18 23:37:37 +0300

    try_1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'key')
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', '').lower() == 'true'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
